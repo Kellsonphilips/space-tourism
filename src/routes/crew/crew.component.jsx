20f617b0f23d0bf1d23e269595750d2163ca6f5b
kellsonphilips <kellsonphilips@gmail.com>
2023-01-10 12:37:33 +0530

    crew mobile modification

--- src/routes/crew/crew.component.jsx
+++ src/routes/crew/crew.component.jsx
@@ -21,52 +21,50 @@ const Crew = ({ crewData }) => {
     .replace(" ", "-")}.png`);
 
   return (
-    <div>
-      <div className="crew-container" key={id}>
-        <div className="crew-info-container">
-          <h2 className="crew-header">
-            <span className="crew-index">02</span>Meet Your Crew
-          </h2>
+    <div className="crew-container" key={id}>
+      <div className="crew-info-container">
+        <h2 className="crew-header">
+          <span className="crew-index">02</span>Meet Your Crew
+        </h2>
 
-          <div className="crew-details-container">
-            <h2 className="crew-role">{role}</h2>
-            <h1 className="crew-name">{name}</h1>
-            <p className="crew-details">{bio}</p>
-          </div>
-          <div className="crew-navigation">
-            <Link
-              className={`crew-navs ${currentCrew === 5 && "active"}`}
-              to="commander"
-              name="5"
-              onClick={handleCrewMemberChange}
-            ></Link>
-            <Link
-              className={`crew-navs ${currentCrew === 6 && "active"}`}
-              to="mission-specialist"
-              name="6"
-              onClick={handleCrewMemberChange}
-            ></Link>
-            <Link
-              className={`crew-navs ${currentCrew === 7 && "active"}`}
-              to="pilot"
-              name="7"
-              onClick={handleCrewMemberChange}
-            ></Link>
-            <Link
-              className={`crew-navs ${currentCrew === 8 && "active"}`}
-              to="flight-engineer"
-              name="8"
-              onClick={handleCrewMemberChange}
-            ></Link>
-          </div>
+        <div className="crew-details-container">
+          <h2 className="crew-role">{role}</h2>
+          <h1 className="crew-name">{name}</h1>
+          <p className="crew-details">{bio}</p>
         </div>
-        <div className="crew-image-container">
-          <img
-            className="image"
-            src={images}
-            alt={`img-${name.toLowerCase().replace(" ", "-")}`}
-          />
+        <div className="crew-navigation">
+          <Link
+            className={`crew-navs ${currentCrew === 5 && "active"}`}
+            to="commander"
+            name="5"
+            onClick={handleCrewMemberChange}
+          ></Link>
+          <Link
+            className={`crew-navs ${currentCrew === 6 && "active"}`}
+            to="mission-specialist"
+            name="6"
+            onClick={handleCrewMemberChange}
+          ></Link>
+          <Link
+            className={`crew-navs ${currentCrew === 7 && "active"}`}
+            to="pilot"
+            name="7"
+            onClick={handleCrewMemberChange}
+          ></Link>
+          <Link
+            className={`crew-navs ${currentCrew === 8 && "active"}`}
+            to="flight-engineer"
+            name="8"
+            onClick={handleCrewMemberChange}
+          ></Link>
         </div>
+      </div>
+      <div className="crew-image-container">
+        <img
+          className="image"
+          src={images}
+          alt={`img-${name.toLowerCase().replace(" ", "-")}`}
+        />
       </div>
     </div>
   );
