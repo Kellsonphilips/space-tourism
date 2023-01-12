c55bbde144faa9b9f005694dd60aa419ad550a68
kellsonphilips <kellsonphilips@gmail.com>
2023-01-12 10:41:06 +0530

    mobile screen modification

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <h2 className="home-title">So, you want to travel to</h2>
         <span className="space">Space</span>
         <p className="welcome-message">
-          So, you want to travel to Space Let’s face it; if you want to go to
+          Let’s face it; if you want to go to
           space, you might as well genuinely go to outer space and not hover
           kind of on the edge of it. Well sit back, and relax because we’ll give
           you a truly out of this world experience!
